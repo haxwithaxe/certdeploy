b09a2421481c7b6b7c9e86250fa9ce6144d0a3a8
haxwithaxe <spam@haxwithaxe.net>
2022-12-14 09:17:20 -0500

    Added ConfigErrorInvalid to help make errors more uniform.

--- src/certdeploy/errors.py
+++ src/certdeploy/errors.py
@@ -5,3 +5,13 @@ class CertDeployError(Exception):
 
 class ConfigError(CertDeployError):
     """Configuration error"""
+
+
+class ConfigErrorInvalid(ConfigError):
+
+    def __init__(self, key, value, must_be=None):
+        if must_be:
+            super().__init__(f'Invalid value "{value}" for `{key}`. `{key}` '
+                             f'must be {must_be}.')
+        else:
+            super().__init__(f'Invalid value "{value}" for `{key}`.')
