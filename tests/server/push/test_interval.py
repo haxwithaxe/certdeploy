39b05cabce6a10fd53c9dfc217048d59e37eff3d
haxwithaxe <spam@haxwithaxe.net>
2023-08-28 01:07:09 -0400

    Added test for server push_interval

---
+++ tests/server/push/test_interval.py
@@ -0,0 +1,64 @@
+"""Tests for the `push_interval` configs.
+
+Both the server and client connection configs.
+"""
+
+from certdeploy.server.server import Server
+
+MAX_SECONDS_OFF = 1
+
+
+def test_tries_on_server_interval_is_timely(
+        mock_fail_client: callable,
+        tmp_server_config: callable,
+        client_conn_config_factory: callable,
+        lineage_factory: callable
+):
+    """Verify that the `push_interval` server config produces tries on an
+    interval.
+    """
+    ## Define some reused variables
+    push_interval = 1
+    push_retry_count = 0
+    total_push_attempts = 1
+    client_address = '127.0.0.1'
+    lineage_name = 'lineage.test'
+    # The filename doesn't matter because it will never get far enough to
+    #   matter.
+    lineage_path = str(lineage_factory(lineage_name, ['doesnotmatter.pem']))
+    ## Setup client
+    client0_server = mock_fail_client(client_address)
+    client0_config = client_conn_config_factory(
+        address=client0_server.address,
+        port=client0_server.port,
+        domains=[lineage_name]
+    )
+    client1_server = mock_fail_client(client_address)
+    client1_config = client_conn_config_factory(
+        address=client1_server.address,
+        port=client1_server.port,
+        domains=[lineage_name]
+    )
+    ## Setup server
+    server_config = tmp_server_config(
+        client_configs=[client0_config, client1_config],
+        push_retries=push_retry_count,
+        push_interval=push_interval
+    )
+    ## Make something to test
+    server = Server(server_config)
+    server.sync(lineage_path, [lineage_name])
+    server.serve_forever(one_shot=True)
+    ## Collect results
+    client0_access_log = client0_server.log
+    client1_access_log = client1_server.log
+    ## Verify results
+    # The log accounts for the first attempt as well as subsequent attempts
+    assert len(client0_access_log) == total_push_attempts
+    assert len(client1_access_log) == total_push_attempts
+    # Now that we know the length is enough get the time difference
+    time_diff = client1_access_log[0] - client0_access_log[0]
+    # The interval is less than the maximum number of seconds
+    assert time_diff.seconds <= push_interval+MAX_SECONDS_OFF
+    # The interval is greater than or equal to the nominal interval
+    assert time_diff.seconds >= push_interval
