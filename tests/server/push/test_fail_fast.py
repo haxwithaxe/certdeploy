c5bf539d8ea4e6d5515ad51ba8faef6a0a6b31cb
haxwithaxe <spam@haxwithaxe.net>
2023-08-28 09:40:21 -0400

    Added fail_fast tests for server push

---
+++ tests/server/push/test_fail_fast.py
@@ -0,0 +1,101 @@
+"""Tests for the `fail_fast` config in push operations."""
+
+import paramiko
+import pytest
+
+from certdeploy.server.server import PushMode, Server
+
+
+def test_fail_fast_on_serial_push(
+        mock_fail_client: callable,
+        tmp_server_config: callable,
+        client_conn_config_factory: callable,
+        lineage_factory: callable
+):
+    """Verify that the `fail_fast` config causes early exit."""
+    ## Define some reused variables
+    client_address = '127.0.0.1'
+    lineage_name = 'lineage.test'
+    # The filename doesn't matter because it will never get far enough to
+    #   matter.
+    lineage_path = str(lineage_factory(lineage_name, ['doesnotmatter.pem']))
+    ## Setup client
+    client0_server = mock_fail_client(client_address)
+    client0_config = client_conn_config_factory(
+        address=client0_server.address,
+        port=client0_server.port,
+        domains=[lineage_name]
+    )
+    client1_server = mock_fail_client(client_address)
+    client1_config = client_conn_config_factory(
+        address=client1_server.address,
+        port=client1_server.port,
+        domains=[lineage_name]
+    )
+    ## Setup server
+    server_config = tmp_server_config(
+        client_configs=[client0_config, client1_config],
+        fail_fast=True,
+        push_mode=PushMode.SERIAL.value
+    )
+    ## Make something to test
+    server = Server(server_config)
+    server.sync(lineage_path, [lineage_name])
+    with pytest.raises(paramiko.ssh_exception.SSHException) as err:
+        server.serve_forever(one_shot=True)
+    # Server raises the right exception given the mock client
+    assert 'Error reading SSH protocol banner' in str(err)
+    # First client has a connection attempt
+    assert len(client0_server.log) == 1
+    # Second client never gets a connection attempt
+    assert len(client1_server.log) == 0
+
+
+def test_fail_fast_on_parallel_push(
+        mock_fail_client: callable,
+        tmp_server_config: callable,
+        client_conn_config_factory: callable,
+        lineage_factory: callable
+):
+    """Verify that the `fail_fast` config causes earlyish exit.
+
+    The expected behavior is that both push workers will finish because they
+    finish so quickly against the mock clients. The first one to be joined
+    will raise an exception.
+    """
+    ## Define some reused variables
+    client_address = '127.0.0.1'
+    lineage_name = 'lineage.test'
+    # The filename doesn't matter because it will never get far enough to
+    #   matter.
+    lineage_path = str(lineage_factory(lineage_name, ['doesnotmatter.pem']))
+    ## Setup client
+    client0_server = mock_fail_client(client_address)
+    client0_config = client_conn_config_factory(
+        address=client0_server.address,
+        port=client0_server.port,
+        domains=[lineage_name]
+    )
+    client1_server = mock_fail_client(client_address)
+    client1_config = client_conn_config_factory(
+        address=client1_server.address,
+        port=client1_server.port,
+        domains=[lineage_name]
+    )
+    ## Setup server
+    server_config = tmp_server_config(
+        client_configs=[client0_config, client1_config],
+        fail_fast=True,
+        push_mode=PushMode.PARALLEL.value
+    )
+    ## Make something to test
+    server = Server(server_config)
+    server.sync(lineage_path, [lineage_name])
+    with pytest.raises(paramiko.ssh_exception.SSHException) as err:
+        server.serve_forever(one_shot=True)
+    # Server raises the right exception given the mock client
+    assert 'Error reading SSH protocol banner' in str(err)
+    # First client has a connection attempt
+    assert len(client0_server.log) == 1
+    # Second client never gets a connection attempt
+    assert len(client1_server.log) == 1
