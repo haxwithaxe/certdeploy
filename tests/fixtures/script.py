2b2efdacc0488fb626c13d6cd6c5b9accf92da21
haxwithaxe <spam@haxwithaxe.net>
2023-08-29 00:28:11 -0400

    Added comment clarifying the return type.

--- tests/fixtures/script.py
+++ tests/fixtures/script.py
@@ -11,5 +11,9 @@ touch {flag_file_path}
 
 @pytest.fixture(scope='function')
 def tmp_script_for_service(tmp_script: callable) -> Any:
-    """Return a script to run and a flag file path to check."""
+    """Return a script to run and a flag file path to check.
+
+    Returns:
+        conftest.Script: A temporary script file and flag file in an object.
+    """
     return tmp_script('script-for-service.sh', SCRIPT_TEMPLATE)
