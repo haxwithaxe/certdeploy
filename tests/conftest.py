d613102bdd63641b7f31bb6b50302562f84a00b3
haxwithaxe <spam@haxwithaxe.net>
2023-08-30 16:41:04 -0400

    Adjusted doc comment

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
-"""
-    Dummy conftest.py for certdeploy.
+"""conftest.py for certdeploy.
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
+Read more about conftest.py under:
+- https://docs.pytest.org/en/stable/fixture.html
+- https://docs.pytest.org/en/stable/writing_plugins.html
 """
 
 
