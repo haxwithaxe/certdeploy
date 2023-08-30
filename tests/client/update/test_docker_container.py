8bd87d4e71eda8a0f157bbbee3402bf80945f5c2
haxwithaxe <spam@haxwithaxe.net>
2023-08-30 16:34:45 -0400

    Added/fixed comments and picked a more specific exception for fail fast test

--- tests/client/update/test_docker_container.py
+++ tests/client/update/test_docker_container.py
@@ -1,12 +1,13 @@
+"""Tests for `certdeploy.client.update.update_docker_container`."""
 
 import time
 
 import pytest
-from conftest import ContainerStatus
+from fixtures.docker_container import ContainerStatus
 
 from certdeploy.client.config.service import DockerContainer
+from certdeploy.client.errors import DockerContainerNotFound
 from certdeploy.client.update import update_docker_container
-from certdeploy.errors import CertDeployError
 
 
 @pytest.mark.docker
@@ -14,8 +15,9 @@ def test_updates_docker_container_by_name(
         canned_docker_container: callable,
         tmp_client_config: callable
 ):
-    """Verify the client can update a container based on the container's
-    name.
+    """Verify the client can update a container.
+
+    Verify the client can update a container based on the container's name.
     """
     client_config = tmp_client_config(
         docker_url='unix://var/run/docker.sock',
@@ -76,7 +78,7 @@ def test_fail_fast_docker_container(
         fail_fast=True
     )
     # Do the thing under test
-    with pytest.raises(CertDeployError):
+    with pytest.raises(DockerContainerNotFound):
         update_docker_container(
             DockerContainer({'filters': {'name': 'doesn\'t exist'}}),
             client_config
