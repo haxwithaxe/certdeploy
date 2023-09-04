484b12d302051c0319002a58de6e56c7fd4b9327
haxwithaxe <spam@haxwithaxe.net>
2023-09-03 23:52:10 -0400

    Added tests for client entrypoint.sh

---
+++ tests/client/docker/test_entrypoint_sh.py
@@ -0,0 +1,42 @@
+"""Tests for the full client docker container.
+
+Note:
+    These tests use the `certdeploy-client:latest` on the host system. If
+    that doesn't have your latest changes then you need to build a new
+    image with `tox -e dockerbuild`.
+"""
+
+import pytest
+
+CLIENT_STARTED_TEXT = (b'INFO:certdeploy-client:Listening for incoming '
+                       b'connections at ')
+CLIENT_HELP_TEXT = b'Usage: certdeploy-client [OPTIONS]'
+
+
+@pytest.mark.certdeploy_docker
+@pytest.mark.docker
+def test_starts_daemon_by_default(client_docker_container: callable):
+    """Verify that the daemon starts by default."""
+    # Setup the client container
+    client = client_docker_container(
+        'entrypoint_sh_default',
+        with_docker=True,
+        config=dict(log_level='DEBUG')
+    )
+    client.start(timeout=300)
+    assert CLIENT_STARTED_TEXT in client.logs()
+
+
+@pytest.mark.certdeploy_docker
+@pytest.mark.docker
+def test_passes_args_to_client(client_docker_container: callable):
+    """Verify that args are passed to the client."""
+    # Setup the client container
+    client = client_docker_container(
+        'entrypoint_sh_args',
+        config=dict(log_level='DEBUG'),
+        entrypoint=['/entrypoint.sh', '--help']
+    )
+    client.start(timeout=None)
+    client.wait_for_log(CLIENT_HELP_TEXT, timeout=300)
+    assert CLIENT_HELP_TEXT in client.logs()
