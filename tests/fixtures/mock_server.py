b3d399c10187eff6732dd45abd33a48d83065c76
haxwithaxe <spam@haxwithaxe.net>
2023-09-04 09:46:40 -0400

    Comment cleanup, typing cleanup, added default for lineage_name

--- tests/fixtures/mock_server.py
+++ tests/fixtures/mock_server.py
@@ -1,7 +1,9 @@
+"""Mock CertDeploy server fixtures and utilities."""
 
 import base64
 import pathlib
 from dataclasses import dataclass
+from typing import Callable
 
 import paramiko
 import pytest
@@ -14,8 +16,9 @@ from certdeploy.server.server import _sftp_mkdir
 
 @dataclass
 class MockPushContext:
+    """A context wrapper for `mock_push_server`."""
 
-    push: callable
+    push: Callable[[], None]
     client_address: str
     client_keypair: KeyPair
     server_keypair: KeyPair
@@ -31,7 +34,18 @@ def _push_to_client(
     lineage_path: pathlib.Path,
     filenames: list[pathlib.Path]
 ):
-    """Push arbitrarily named certs to a client."""
+    """Push arbitrarily named certs to a client.
+
+    Arguments:
+        client_path: The path on the client to dump the lineage in.
+        client_address: The listen address of the CertDeploy client.
+        client_port: The listen port of the CertDeploy client.
+        client_keypair: The key pair for the CertDeploy client.
+        server_keypair: The key pair for the CertDeploy server.
+        lineage_path: The local path of the lineage.
+        filenames: A list of filenames to transfer from `lineage_path` to
+            `client_path`
+    """
     cert_dir = client_path.joinpath(lineage_path.name)
     ssh = paramiko.client.SSHClient()
     if client_port == 22:
@@ -60,8 +74,10 @@ def _push_to_client(
 
 @pytest.fixture()
 def mock_server_push(keypairgen: callable, lineage_factory: callable,
-                     tmp_path: pathlib.Path) -> callable:
-    """Return the push context with a function to push to the client."""
+                     tmp_path: pathlib.Path
+                     ) -> Callable[[dict, pathlib.Path, str, KeyPair, KeyPair,
+                                    str, list[pathlib.Path]], MockPushContext]:
+    """Return a mock CertDeploy server push context factory."""
 
     def _mock_server_push(
         client_config: dict,
@@ -72,8 +88,30 @@ def mock_server_push(keypairgen: callable, lineage_factory: callable,
         lineage_name: str = None,
         filenames: list[pathlib.Path] = None
     ) -> MockPushContext:
-        """Prepare to push arbitrarily named certs to a client."""
-        ## Reconsile the variables
+        """Prepare to push arbitrarily named certs to a client.
+
+        Arguments:
+            client_config: CertDeploy client config `dict`.
+            client_path: The path on the client to dump the lineage in.
+                Defaults to a new temporary directory.
+            client_address: The listen address of the CertDeploy client.
+                Defaults to the configured SFTPD `listen_address`.
+            client_port: The listen port of the CertDeploy client. Defaults to
+                the configured SFTPD `listen_port`.
+            client_keypair: The key pair for the CertDeploy client. Defaults to
+                a freshly generated `KeyPair`.
+            server_keypair: The key pair for the CertDeploy server. Defaults to
+                a freshly generated `KeyPair`.
+            lineage_name: The name (not the path) of the lineage to generate.
+                Defaults to `'test.example.com'`.
+            filenames: A list of filenames to transfer from `lineage_path` to
+                `client_path`. Defaults to `['fullchain.pem', 'privkey.pem']`.
+
+        Returns:
+            A context object with the pusher function, key pairs, lineage
+            filenames, and client address.
+        """
+        ## Reconcile the variables
         # Calling out the places where things are added to the context so they
         #   don't get lost in the noise.
         client_path = client_path or ContainerInternalPaths.source
@@ -88,7 +126,8 @@ def mock_server_push(keypairgen: callable, lineage_factory: callable,
         )
         server_keypair = server_keypair or default_server_keypair
         filenames = filenames or ['fullchain.pem', 'privkey.pem']
-        lineage_path = lineage_factory(lineage_name, filenames)
+        lineage_path = lineage_factory(lineage_name or 'test.example.com',
+                                       filenames)
         if not client_address:
             client_address = client_config['sftpd']['listen_address']
         if client_address == '0.0.0.0':
