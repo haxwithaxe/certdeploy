f5c818a728bafa53a83f18f7fcdeb73d8f2c9f94
haxwithaxe <spam@haxwithaxe.net>
2025-08-22 14:28:46 -0400

    Added ghcr.io to docker publishing

--- docker/publish.sh
+++ docker/publish.sh
@@ -5,6 +5,7 @@
 set -e
 
 DEST_IMAGE_PREFIX="haxwithaxe/certdeploy"
+ALT_DEST_IMAGE_PREFIX="ghcr.io/$DEST_IMAGE_PREFIX"
 SRC_IMAGE_PREFIX="certdeploy"
 TAG=${1:-${TAG:-$(git tag --points-at HEAD)}}
 
@@ -16,21 +17,29 @@ fi
 echo Tagging ${SRC_IMAGE_PREFIX}-server as $TAG
 if [[ "$TAG" != *"-"* ]]; then
 	docker image tag ${SRC_IMAGE_PREFIX}-server:latest ${DEST_IMAGE_PREFIX}-server:latest
+	docker image tag ${SRC_IMAGE_PREFIX}-server:latest ${ALT_DEST_IMAGE_PREFIX}-server:latest
 fi
 docker image tag ${SRC_IMAGE_PREFIX}-server:latest ${DEST_IMAGE_PREFIX}-server:$TAG
+docker image tag ${SRC_IMAGE_PREFIX}-server:latest ${ALT_DEST_IMAGE_PREFIX}-server:$TAG
 echo Pushing ${DEST_IMAGE_PREFIX}-server latest and $TAG
 if [[ "$TAG" != *"-"* ]]; then
 	docker image push ${DEST_IMAGE_PREFIX}-server:latest
+	docker image push ${ALT_DEST_IMAGE_PREFIX}-server:latest
 fi
 docker image push ${DEST_IMAGE_PREFIX}-server:$TAG
+docker image push ${ALT_DEST_IMAGE_PREFIX}-server:$TAG
 
 echo Tagging ${SRC_IMAGE_PREFIX}-client as $TAG
 if [[ "$TAG" != *"-"* ]]; then
 	docker image tag ${SRC_IMAGE_PREFIX}-client:latest ${DEST_IMAGE_PREFIX}-client:latest
+	docker image tag ${SRC_IMAGE_PREFIX}-client:latest ${ALT_DEST_IMAGE_PREFIX}-client:latest
 fi
 docker image tag ${SRC_IMAGE_PREFIX}-client:latest ${DEST_IMAGE_PREFIX}-client:$TAG
+docker image tag ${SRC_IMAGE_PREFIX}-client:latest ${ALT_DEST_IMAGE_PREFIX}-client:$TAG
 echo Pushing ${DEST_IMAGE_PREFIX}-client latest and $TAG
 if [[ "$TAG" != *"-"* ]]; then
 	docker image push ${DEST_IMAGE_PREFIX}-client:latest
+	docker image push ${ALT_DEST_IMAGE_PREFIX}-client:latest
 fi
 docker image push ${DEST_IMAGE_PREFIX}-client:$TAG
+docker image push ${ALT_DEST_IMAGE_PREFIX}-client:$TAG
